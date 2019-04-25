2cf0a67d3b63d168f3ee1076b8d940950ba41a78
alexelnino <alexel_nino@yahoo.com>
2019-04-25 11:21:49 +0700

    code2

--- belajarflask/git.py
+++ belajarflask/git.py
@@ -1,7 +1,10 @@
 print('hahahaha')
+print('yuhuuu')
 
 #git init
 #git status
 #git add .  ==> meng add semua file ke dalam folder
 #git add namaFile  ==>menambahkan satu file ke dalam folder
 #git commit -m "......"=>folder yang tadi di commit/ diberi label ....=>label
+#git log=>untuk melihat history
+#git log --oneline=>untuk melihat history 1 baris
